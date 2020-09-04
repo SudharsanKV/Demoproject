5474c711e220f1467ed27f1cf3c1560cf1aa0f8c
SudharsanKV <sudharsanndanav@gmail.com>
2020-09-04 08:50:06 +0530

    Added scanner and integer and printing the value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello");
+        Scanner sc= new Scanner(System.in);
+        int value=sc.nextInt();
+        System.out.println(value);
+
     }
 }
